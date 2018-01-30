79738d951517c73b5a5373d98dcac77c8fcc0e0f
Alex <Alex@Alex-PC>
2018-01-30 13:57:16 +0300

    выборка данных

---
+++ src/main/java/ru/alexander/hibernate/sample/AuthorHelper.java
@@ -0,0 +1,65 @@
+package ru.alexander.hibernate.sample;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import ru.alexander.hibernate.sample.entity.Author;
+
+
+public class AuthorHelper {
+
+    private SessionFactory sessionFactory;
+
+    public AuthorHelper() {
+        sessionFactory = HibernateUtil.getSessionFactory();
+    }
+
+    public List<Author> getAuthorList(){
+
+        // открыть сессию - для манипуляции с персист. объектами
+        Session session = sessionFactory.openSession();
+
+        Author author = session.get(Author.class, 1L); // получение объекта по id
+
+
+        System.out.println(author.getName());
+
+
+        // этап подготовки запроса
+
+        // объект-конструктор запросов для Criteria API
+        CriteriaBuilder cb = session.getCriteriaBuilder();// не использовать session.createCriteria, т.к. deprecated
+
+        CriteriaQuery cq = cb.createQuery(Author.class);
+
+        Root<Author> root = cq.from(Author.class);// первостепенный, корневой entity (в sql запросе - from)
+
+        cq.select(root);// необязательный оператор, если просто нужно получить все значения
+
+
+
+        // этап выполнения запроса
+        Query query = session.createQuery(cq);
+
+        List<Author> authorList = query.getResultList();
+
+        session.close();
+
+        return authorList;
+
+    }
+
+
+    public Author getAuthor(String name){
+        return null;
+    }
+
+
+
+}
--- src/main/java/ru/alexander/hibernate/sample/Start.java
+++ src/main/java/ru/alexander/hibernate/sample/Start.java
@@ -1,12 +1,15 @@
 package ru.alexander.hibernate.sample;
 
 import org.hibernate.Session;
+import ru.alexander.hibernate.sample.entity.Author;
 
 public class Start {
-
 
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
 
+        for (Author item : new AuthorHelper().getAuthorList()) {
+            System.out.println(item.getName());
+        }
     }
 }
